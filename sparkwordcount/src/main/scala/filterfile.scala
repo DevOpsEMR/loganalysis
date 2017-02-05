3f446b15ddadf39af569522475613c74cbdfcb97
schakkot <schakkot@BLRWFL20181.corp.capgemini.com>
2017-02-05 11:42:03 +0530

    Initial Commit

---
+++ sparkwordcount/src/main/scala/filterfile.scala
@@ -0,0 +1,16 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object filterfile {  
+  def main(args: Array[String]) {
+   val logFile = "D:/Drive/BigData/CONA/Data/sample.txt"
+  // val conf = new SparkConf().setAppName("Spark Wordcount")
+   val conf = new SparkConf().setAppName("Spark Wordcount").setMaster("local[2]").set("spark.executor.memory","1g");
+   val sc = new SparkContext(conf)
+   val logData = sc.textFile(logFile, 2).cache()
+   val numAs = logData.filter(line => line.contains("ERROR")).count()
+   val numBs = logData.filter(line => line.contains("WARNING")).count()
+     println("Lines with ERROR: %s, Lines with WARNING: %s".format(numAs, numBs))     
+  }
+}
