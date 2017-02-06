a9f18696350987a81ff5f753e659d2cdb007c266
schakkot <schakkot@BLRWFL20181.corp.capgemini.com>
2017-02-06 12:23:32 +0530

    Updated

--- sparkwordcount/src/main/scala/filterfile.scala
+++ sparkwordcount/src/main/scala/filterfile.scala
@@ -5,15 +5,24 @@ import org.apache.spark.SparkConf
 
 object filterfile {
   def main(args: Array[String]) {
+    
     val conf = new SparkConf().setAppName("Spark Wordcount").setMaster("local[2]").set("spark.executor.memory","1g");
     val sc = new SparkContext(conf)
-    val pathToFiles = "https://s3.amazonaws.com/devopsemr/commafile.txt"
-    val outputPath = "https://s3.amazonaws.com/devopsemr/output.txt"
+    //
+    val hadoopConf = sc.hadoopConfiguration
+    hadoopConf.set("fs.s3.impl", "org.apache.hadoop.fs.s3native.NativeS3FileSystem")
+    hadoopConf.set("fs.s3.awsAccessKeyId", "AKIAJBGUMHBIOXCXMCWA")
+    hadoopConf.set("fs.s3.awsSecretAccessKey", "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")
+    //
+    
+    val pathToFiles = "s3://devopsemr/commafile.txt"
+    val outputPath = "s3://devopsemr/output.txt"
     val files = sc.textFile(pathToFiles)
    
     val rowsWithoutSpaces = files.map(_.replaceAll(" ", ","))
     
-    rowsWithoutSpaces.saveAsTextFile(outputPath)
+    //rowsWithoutSpaces.saveAsTextFile(outputPath)
+    files.saveAsTextFile(outputPath)
   }
 
 }
